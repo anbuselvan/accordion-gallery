2403d50daed189558b51538a2f83cbbc70f7d368
Anbuselvan Periannan <anbuselvan.p@gmail.com>
2015-04-19 15:02:31 -0700

    updated package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'anbuselvan:accordion-gallery',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Accorion image gallery',
   // URL to the Git repository containing the source code for this package.
